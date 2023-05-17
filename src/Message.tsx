5e009f20de31ed32667d46f6286e3c24620238a1
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-17 11:24:44 +0530

    creating a React Component

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// PasacalCasing ex: AjayMaurya
+function Message(){
+    // JSX Javascript XML
+    const name= 'Mosh'
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+export default Message
