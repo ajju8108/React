165c7dac4076a07fa2d0836eac60102afa1f3e35
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-17 12:41:21 +0530

    creating  a listgroup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Message from "./Message"
+import ListGroup from "./components/ListGroup"
+
 function App() {
   return (
     <div className="App">
-      <Message/>
+     <ListGroup/>
     </div>
   )
 }
