af080dee3e3b37ed774c8020a1b30f21dd53dab5
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-18 16:11:59 +0530

    Updating nested objects

--- src/Managing State/Updating Objects/UpdateObject.tsx
+++ src/Managing State/Updating Objects/UpdateObject.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
-
+// Updating Objects and nested object
 export default function UpdateObject() {
-  const [drink, setDrink] = useState({
+  /* const [drink, setDrink] = useState({
     title: "Americanoi",
     price: 5,
+  }); */
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 19911,
+    },
   });
   const handleClick = () => {
-    setDrink({ ...drink, price: 8 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 9999 },
+    });
   };
   return (
     <div>
-      {drink.price}
+      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
