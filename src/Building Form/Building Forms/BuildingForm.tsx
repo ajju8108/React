570581f67d9bdd0ef09f686af6a807cccd64e008
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-19 11:38:25 +0530

    Accessing input fiels

--- src/Building Form/Building Forms/BuildingForm.tsx
+++ src/Building Form/Building Forms/BuildingForm.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FormEvent } from "react";
 
 export default function BuildingForm() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    name: "",
+    age: 0,
+  };
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    // console.log("Submitted");
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value);
+    }
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -12,13 +25,13 @@ export default function BuildingForm() {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input id="name" ref={nameRef} type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input id="age" ref={ageRef} type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
