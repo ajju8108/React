2f46358c9cbc7956acae44e1847b010737718774
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-22 12:27:07 +0530

    woeking with asyn and await

--- src/Connecting to backend/Fetching/FetchingData.tsx
+++ src/Connecting to backend/Fetching/FetchingData.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
 interface User {
   id: number;
@@ -9,10 +9,21 @@ export default function FetchingData() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchUsers();
+    // axios
+    //   .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+    //   .then((res) => setUsers(res.data))
+    //   .catch((err) => setError(err.message));
   }, []);
   return (
     <>
@@ -24,4 +35,7 @@ export default function FetchingData() {
       </ul>
     </>
   );
+}
+function aysnc() {
+  throw new Error("Function not implemented.");
 }
