dc4e5abfdec705b068297fab7bf2c2c3c974240e
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-22 12:43:03 +0530

    Showing a Loading Inidcator

--- src/Connecting to backend/Fetching/FetchingData.tsx
+++ src/Connecting to backend/Fetching/FetchingData.tsx
@@ -8,6 +8,7 @@ interface User {
 export default function FetchingData() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     /* const fetchUsers = async () => {
       try {
@@ -21,20 +22,27 @@ export default function FetchingData() {
     };
     fetchUsers(); */
     const controller = new AbortController();
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
+
     return () => controller.abort();
   }, []);
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
